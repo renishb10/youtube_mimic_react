5708e3bfd7b5088387c1ba6ae797c59963cdc792
Renish B <renishb10@gmail.com>
2019-08-07 23:37:16 +0530

    added new component statusbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       Hello world
+      <SearchBar/>
     </div>
   );
 }
